a92f4b827e857667c88ae0436f7c78f244b5912f
zhuhaibao <731826228@qq.com>
2018-04-20 15:37:42 +0800

    213

--- jenkins/run_alltest.py
+++ jenkins/run_alltest.py
@@ -8,30 +8,29 @@ sys.setdefaultencoding('utf-8')
 sys.path.append(r'C:\Users\test\Desktop\jenkins\Base')
 from CreatePhoneNum import *
 #用例路径
-print os.getcwd()    #获取当前工作目录
+# print os.getcwd()    #获取当前工作目录
 case_pase=os.path.join(os.getcwd(),"testcase")  #加入路径
 
 #用例路径
 
 report_path=os.path.join(os.getcwd(),"report")
-print case_pase
+# print case_pase
 def all_case():
     discover=unittest.defaultTestLoader.discover(case_pase,
                                                  pattern="test*.py",
                                                  top_level_dir=None)
-    print(discover)
     return discover
 
 if __name__=="__main__":
 
     # 获取当期时间，这样便于下面的使用
-    now=time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
+    # now=time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
 
     # html报告存放路径
-    report_abspath=os.path.join(report_path,"result_"+now+".html")
+    # report_abspath=os.path.join(report_path,"result_" + ".html")
 
     # 打开一个文件，将result写入此file中
-    fp=open(report_abspath,"ab+")
+    fp=open('report.html',"ab+")
     runner=HTMLTestRunner.HTMLTestRunner(stream=fp,
                                          title=u'接口Zion规划测试报告，结果如下：',
                                          description=u'用例执行情况：')
