c941f13f3e628817a829d910ba94b61e9eff9565
zhuhaibao <731826228@qq.com>
2018-04-20 14:40:51 +0800

    带有测试报告

---
+++ jenkins/run_alltest.py
@@ -0,0 +1,35 @@
+#coding=utf-8
+import unittest
+import os,time
+import HTMLTestRunner
+#用例路径
+print os.getcwd()    #获取当前工作目录
+case_pase=os.path.join(os.getcwd(),"testcase")  #加入路径
+
+#用例路径
+
+report_path=os.path.join(os.getcwd(),"report")
+print case_pase
+def all_case():
+    discover=unittest.defaultTestLoader.discover(case_pase,
+                                                 pattern="test*.py",
+                                                 top_level_dir=0)
+    print(discover)
+    return discover
+
+if __name__=="__main__":
+
+    # 获取当期时间，这样便于下面的使用
+    now=time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
+
+    # html报告存放路径
+    report_abspath=os.path.join(report_path,"result_"+now+".html")
+
+    # 打开一个文件，将result写入此file中
+    fp=open(report_abspath,"wb")
+    runner=HTMLTestRunner.HTMLTestRunner(stream=fp,
+                                         title=u'接口Zion规划测试报告，结果如下：',
+                                         description=u'用例执行情况：')
+    # runner=unittest.TextTestRunner()
+    runner.run(all_case())
+    fp.close()
